4ba17a2e5f227b66e6bbbc8dc9dd7b252e148370
Buddy Magsipoc <keikun17@gmail.com>
2014-06-12 16:17:11 +0800

    +backbonejs。

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -3,9 +3,22 @@ require.config({
   paths: {
     "jquery": "./../components/jquery/dist/jquery",
     "react": "./../components/react/react",
-    "underscore": "./../components/underscore/underscore"
+    "underscore": "./../components/underscore/underscore",
+    "backbone": "./../componentss/backbone/backbone"
+  },
+  shims: {
+    "backbone": {
+      deps: ["jquery", "underscore"],
+      exports: "Backbone"
+    },
+    "jquery": {
+      "exports": "$"
+    },
+    "underscore": {
+      "exports": "_"
+    }
   }
-})
+});
 
 
 define(function(require){
