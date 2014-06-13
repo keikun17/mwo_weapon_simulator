9ccf947fac0d33d3d6c8a3bcbf1c8820acbed974
Buddy Magsipoc <keikun17@gmail.com>
2014-06-14 00:52:57 +0800

    WIP : Weapon loading。

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -41,12 +41,45 @@ define(function(require){
     }
   });
 
+  // Router
+
   var app_router = new Router;
 
   app_router.on('route:goHome', function(){
     console.log("welcome");
   });
 
+
+  // Component
+  var weaponComponent = react.createClass({
+    handleClick: function(){
+      alert('weapon clicked!');
+    },
+    render: function(){
+      return(
+        // <a href="#" onClick={this.handleClick}>Medium Laser</a>'
+        react.DOM.a({href: '#'}, "Medium Las")
+      )
+    }
+  });
+
+  // View
+  var weaponView = Backbone.View.extend({
+    initialize: function(){
+      this.render();
+    },
+
+    render: function(){
+      console.log("rendering weapon");
+      console.log(this.$el)
+      return this
+    }
+  })
+
+  var medium_laser = new weaponView({el: '#equipped-weapons'});
+  medium_laser.render();
+
+  // Always at the end
   Backbone.history.start();
 })
 
