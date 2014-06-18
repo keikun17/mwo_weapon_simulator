d3b205cd7db77b8bd3fada75cee58e6d4cee6d1c
Buddy Magsipoc <keikun17@gmail.com>
2014-06-18 20:06:56 +0800

    Use collections。

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -54,73 +54,57 @@ define(function(require){
 
   // Model
   var Weapon = Backbone.Model.extend({
+    defaults: {
+      name: 'WEAPON NAME',
+      damage: 0,
+      cooldown: 0,
+      heat: 0,
+      heatgroup: "HEAT GROUP"
+    },
     initialize: function(){
       console.log("Initializing weapon");
     }
   });
 
-  var small_laser = new Weapon({
-    name: "Small Laser",
-    damage: "5",
-    cooldown: "3",
-    heat: "2",
-    heatgroup: "lasers"
-  });
-
-  var medium_laser = new Weapon({
-    name: "Medium Laser",
-    damage: "5",
-    cooldown: "3",
-    heat: "2",
-    heatgroup: "lasers"
-  });
-
-  var large_laser = new Weapon({
-    name: "Large Laser",
-    damage: "5",
-    cooldown: "3",
-    heat: "2",
-    heatgroup: "lasers"
-  });
-
-  // Collection
-  var weaponCollection = Backbone.Collection.extend({
-    model: Weapon
-  });
-
-  var InnerSphereWeapons = new weaponCollection([small_laser, medium_laser, large_laser]);
-
   // Component
   var weaponComponent = react.createClass({
+
     handleClick: function(){
       alert('weapon clicked!');
     },
+
     render: function(){
+      console.log("props are ");
+      console.log(this.props);
       return(
         /** @jsx React.DOM */
         react.DOM.a({ href: '#', className: 'btn btn-primary', onClick: this.handleClick }, this.props.name)
         // <a href="#" onClick={this.handleClick}>Medium Laser</a>'
       )
     }
+
   });
 
   // View
-  var weaponView = Backbone.View.extend({
-    initialize: function(options){
-      this.options = options || {};
+  var WeaponView = Backbone.View.extend({
+    initialize: function(){
+      console.log("This model is");
+      console.log(this.model);
+      console.log("JSON of the model is");
+      console.log(this.model.toJSON())
       this.render();
     },
 
     render: function(){
       console.log("rendering weapon");
-      console.log(this.$el)
+      weapon_attrs = this.model.toJSON();
       react.renderComponent(new weaponComponent({
-        name: this.options.weapon_name,
+        name: weapon_attrs.name,
         handleClick: this.clickHandler.bind(this)
-      }),
-      document.getElementById('weaponselect'));
+      }), document.getElementById('weaponselect'));
       return this
     },
+
     clickHandler: function() {
       alert("Weapon click overridden by view");
     }
@@ -128,8 +112,70 @@ define(function(require){
   })
 
 
-  var medium_laser_view = new weaponView({weapon_name: 'Medium Laser'});
-  medium_laser_view.render();
+  // TODO: Should be rendered by a collectionview
+  // var medium_laser_view = new weaponView({weapon_name: 'Medium Laser'});
+  // var medium_laser_view = new weaponView({weapon_name: 'Medium Laser'});
+  // var medium_laser_view = new weaponView({weapon_name: 'Medium Laser'});
+
+  // Collection
+  var EquippedWeaponsCollection = Backbone.Collection.extend({
+    model: Weapon
+  })
+
+  // CollectionViews
+  var EquippedWeaponsCollectionView = Backbone.View.extend({
+
+    initialize: function(){
+      console.log("The collection is");
+      console.log(this.collection);
+
+    },
+
+    render: function(){
+      // Should loop through all weapons in the collection and render them
+      // on the equipped weapons container
+
+      // so we don't lost context of the instance once we go to the loop
+      this.collection.each(function(weapon){
+        console.log('rendering weapon : ');
+        console.log(weapon);
+
+        var weaponView = new WeaponView({model: weapon})
+      });
+      return this
+    }
+  });
+
+  var small_laser = {
+    name: "Small Laser",
+    damage: "5",
+    cooldown: "3",
+    heat: "2",
+    heatgroup: "lasers"
+  };
+
+  var medium_laser = {
+    name: "Medium Laser",
+    damage: "5",
+    cooldown: "3",
+    heat: "2",
+    heatgroup: "lasers"
+  };
+
+  var large_laser = {
+    name: "Large Laser",
+    damage: "5",
+    cooldown: "3",
+    heat: "2",
+    heatgroup: "lasers"
+  };
+
+  console.log("initializing collection");
+  var equippedWeaponsCollection = new EquippedWeaponsCollection([small_laser, medium_laser, large_laser]);
+
+  console.log("pass collection to collection view");
+  var equippedWeaponsCollectionView = new EquippedWeaponsCollectionView({collection: equippedWeaponsCollection})
+  equippedWeaponsCollectionView.render();
 
   // Always at the end
   Backbone.history.start();
