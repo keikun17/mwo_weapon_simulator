341745b60e1cb02de868de2d8632026b0412d36d
Buddy Magsipoc <keikun17@gmail.com>
2014-06-20 17:23:00 +0800

    Separate config。

--- source/javascripts/app.js
+++ source/javascripts/app.js
@@ -1,29 +1,3 @@
-// Configuration
-require.config({
-  // CBust cache for every reload
-  // TODO : bust cache only for local/development
-  urlArgs: "bust=" + (new Date()).getTime(),
-  paths: {
-    "jquery": "./../components/jquery/dist/jquery",
-    "react": "./../components/react/react",
-    "underscore": "./../components/underscore/underscore",
-    "backbone": "./../components/backbone/backbone"
-  },
-  shims: {
-    "backbone": {
-      deps: ["jquery", "underscore"],
-      exports: "Backbone"
-    },
-    "jquery": {
-      "exports": "$"
-    },
-    "underscore": {
-      "exports": "_"
-    }
-  }
-});
-
-
 define(function(require){
   var
   // library deps
---
+++ source/javascripts/config.js
@@ -0,0 +1,26 @@
+// Configuration
+require.config({
+  // CBust cache for every reload
+  // TODO : bust cache only for local/development
+  urlArgs: "bust=" + (new Date()).getTime(),
+  paths: {
+    "jquery": "./../components/jquery/dist/jquery",
+    "react": "./../components/react/react",
+    "underscore": "./../components/underscore/underscore",
+    "backbone": "./../components/backbone/backbone"
+  },
+  shims: {
+    "backbone": {
+      deps: ["jquery", "underscore"],
+      exports: "Backbone"
+    },
+    "jquery": {
+      "exports": "$"
+    },
+    "underscore": {
+      "exports": "_"
+    }
+  }
+});
+
+
